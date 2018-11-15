5c5a3548d94fe1a08527b9544f54ef36a4283a6d
Emerson Doyah <emerson.doyah@gmail.com>
2018-11-15 08:45:18 -0600

    initial commit

---
+++ src/main/resources/static/script/app.js
@@ -0,0 +1,46 @@
+// autosize(document.getElementById("commentInput"));
+// autosize(document.getElementById("postTitleInput"));
+// autosize(document.getElementById("postTextInput"));
+// autosize(document.getElementById("replyInput"));
+// autosize(document.getElementById("editPostTitleInput"));
+// autosize(document.getElementById("editPostTextInput"));
+
+// $("#commentForm").submit(function(event) {
+	// event.preventDefault();
+	//
+	// // create new friend to be submitted
+	// var comment = {
+	// text : $("#commentInput").val(),
+	// postId : $("#postId").val()
+	//
+	// };
+	//
+	// // AJAX post the data to the friends API.
+	// $.post("/post/addComment", comment, function(post) {
+	// // Grab the best matched from the AJAX response.
+	// $("#matchedName").text(match.name);
+	// $("#score").text(match.score);
+	// $("#matchedImage").attr("src", match.image);
+	// });
+	// $("#largeModal").modal("toggle");
+	//
+// });
+
+// $("#cancelButton").click(function() {
+// 	$("#commentInput").val("");
+// });
+//
+// $("#reply-link").click(function() {
+// 	$("#replyForm").removeClass("d-none");
+// });
+//
+// $(function() {
+// 	$(document).scroll(function() {
+// 		var $nav = $("#mainNav");
+// 		if ($(window).scrollTop() > $nav.height()) {
+// 			$nav.addClass("bg-dark");
+// 		} else {
+// 			$nav.removeClass("bg-dark");
+// 		}
+// 	})
+// })
