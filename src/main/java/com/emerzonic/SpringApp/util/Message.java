73f2dcfe3ab3363cb2687d8c33de8d96aff5384d
Emerson Doyah <emerson.doyah@gmail.com>
2018-11-20 22:09:36 -0600

    added getter and setter for message class

--- src/main/java/com/emerzonic/SpringApp/util/Message.java
+++ src/main/java/com/emerzonic/SpringApp/util/Message.java
@@ -7,4 +7,12 @@ public class Message {
     public Message(String text) {
         this.text = text;
     }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
